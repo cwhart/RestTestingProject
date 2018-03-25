92f3eaab6778da66e75e76de5e40b90339da48ba
Cindy Hartnett <cynthia.hartnett@libertymutual.com>
2018-03-25 09:31:46 -0700

    Completed Dog Breeds

--- DogGenetics/src/main/java/DogGenetics.java
+++ DogGenetics/src/main/java/DogGenetics.java
@@ -8,17 +8,42 @@ public class DogGenetics {
         Scanner userInput = new Scanner(System.in);
         String dogsName = "";
 
-        String[] dogBreeds = new String[]{"Chihuahua", "Schnauzer", "Coonhound", "Poodle", "Daschund"};
+        //List of dog breeds to randomly select from
+        String[] dogBreedsToSelect = new String[]{"Chihuahua", "Schnauzer", "Coonhound", "Poodle", "Daschund", "German Shepherd",
+        "Labrador Retriever", "Rottweiler", "Golden Retriever", "Beagle", "Bulldog", "Husky", "Boxer", "Pug", "Border Collie",
+        "Shih Tzu", "Pit Bull", "Sheepdog", "Basset Hound", "Corgi"};
+
+        //Select 5 at random and populate here
+        String[] dogBreeds = new String[5];
         int[] percentages = new int[5];
         int runningTally = 95;
         int sum = 0;
         Random rand = new Random();
 
+        //Prompt the user for their dog's name
         System.out.println("What is your dog's name? ");
         dogsName = userInput.nextLine();
         System.out.println("Well then, I have this highly reliable report on " + dogsName +
                 "'s prestigious background right here.");
 
+        //Loop through the 20 breeds and choose 5 at random
+        for(int j=0; j<5; j++) {
+            int thisRandom;
+
+            //Generate a random index for selecting the breed from the array.
+            //Once selected, set it to mull so it doesn't get selected again.
+            //And before selecting, check to ensure it's not equal to mull.
+            do {
+                thisRandom = rand.nextInt(20);
+            } while (dogBreedsToSelect[thisRandom] == null);
+
+                dogBreeds[j] = dogBreedsToSelect[thisRandom];
+                dogBreedsToSelect[thisRandom] = null;
+
+        }
+
+        //Loop through and generate 4 random percentages. Upper bound for the first percentage is 95.
+        //Each time, decrement the upper bound as each percentage is generated.
         for(int i=0; i<4; i++) {
 
             int thisPercentage = rand.nextInt(runningTally) + 1;
@@ -29,7 +54,7 @@ public class DogGenetics {
             System.out.println(dogsName + " is " + percentages[i] + " percent " + dogBreeds[i]);
         }
 
-        //In order to total to 100, the final percentage cannot be random, it must equal the remainder.
+        //The final percentage must be 100 minus the sum of the 4 previous, to ensure it all adds up to 100.
         percentages[4] = 100 - sum;
         System.out.println(dogsName + " is " + percentages[4] + " percent " + dogBreeds[4]);
 
